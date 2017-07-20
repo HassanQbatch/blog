76eb4890fab3ae71f5a7115eb97dd50645b13f07
HassanQbatch <hassan.tariq@qbatch.com>
2017-07-20 17:05:09 +0500

    Employee New Form and Actions Added

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,19 @@
 class EmployeesController < ApplicationController
+	def new
+		@employee = Employee.new
+	end
+
+	def create
+		@employee = Employee.new(employee_params)
+		if @employee.save
+			redirect_to @employee
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def employee_params
+		params.require(:employee).permit(:name, :gender, :email, :password, :joining_date)
+	end
 end
